c927441dc5db9dd4a2caecd1180865ff9733663a
Roger Oliveira <rogerpolvr@gmail.com>
2022-12-24 20:17:42 -0300

    feat: create categories route

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
+
+const port = 3333;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
